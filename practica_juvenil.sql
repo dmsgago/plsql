65fe1d6da721c26650041f9d54579d16f1a6be2d
Diego Martín Sánchez <dmsgago@gmail.com>
2015-03-20 01:01:17 +0100

    Script practica juvenil añadido

---
+++ practica_juvenil.sql
@@ -0,0 +1,5 @@
+/*Diseña un procedimiento al que pasemos como parámetro de entrada el nombre de uno de los módulos existentes en la BD y visualice el nombre de los alumnos que lo han cursado junto a su nota.
+Al final del listado debe aparecer el nº de suspensos, aprobados, notables y sobresalientes.
+Asimismo, deben aparecer al final los nombres y notas de los alumnos que tengan la nota más alta y la más baja.
+Debes comprobar que las tablas tengan almacenada información y que exista el módulo cuyo nombre pasamos como parámetro al procedimiento. */
+
