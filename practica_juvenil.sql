bbfde70e7e7232e70399b2e4b70480d1280313d1
Diego Martín Sánchez <dmsgago@gmail.com>
2015-03-20 13:50:29 +0100

    modificación y acabado de los procedimientos
    modificación del procedimiento ContabilizarTipoNota

--- practica_juvenil.sql
+++ practica_juvenil.sql
@@ -44,30 +44,22 @@ END principal;
 CREATE OR REPLACE PROCEDURE MostrarAlumnoYNota(p_nombre alumnos.apenom%type, p_nota notas.nota%type)
 IS
 BEGIN
-	dbms_output.put_line(p_nombre || p_nota);
+	dbms_output.put_line('ALUMNO: ' || p_nombre || '. NOTA: ' || p_nota);
 END  MostrarAlumnoYNota;
 /
 
 CREATE OR REPLACE PROCEDURE ContabilizarTipoNota (p_nota notas.nota%type, p_sus IN OUT NUMBER, p_ap IN OUT NUMBER, p_not IN OUT NUMBER, p_sob IN OUT NUMBER)
 IS
-	v_suspenso NUMBER:=0;
-	v_aprobado NUMBER:=0;
-	v_notable NUMBER:=0;
-	v_sobresaliente NUMBER:=0;
 BEGIN
 	IF p_nota < 5 THEN
-		v_suspenso:=v_suspenso+1;
+		p_sus:=p_sus+1;
 	ELSIF p_nota < 7 THEN
-		v_aprobado:=v_aprobado+1;
+		p_ap:=p_ap+1;
 	ELSIF p_nota < 9 THEN
-		v_notable:=v_notable+1;
+		p_not:=p_not+1;
 	ELSE
-		v_sobresaliente:=v_sobresaliente+1;
+		p_sob:=p_sob+1;
 	END IF;
-	p_sus:=v_suspenso;
-	P_ap:=v_aprobado;
-	p_not:=v_notable;
-	p_sob:=v_sobresaliente;
 END  ContabilizarTipoNota;
 /
 
@@ -94,6 +86,6 @@ END ComprobarSiMin;
 CREATE OR REPLACE PROCEDURE MostrarResultados(p_cont_sus NUMBER, p_cont_ap NUMBER, p_cont_not NUMBER, p_cont_sob NUMBER, p_nom_max VARCHAR2, p_nota_max NUMBER, p_nom_min VARCHAR2, p_nota_min NUMBER)
 IS
 BEGIN
-	dbms_output.put_line(p_cont_sus || p_cont_ap || p_cont_not || p_cont_sob || p_nom_max || p_nota_max || p_nom_min || p_nota_min);
+	dbms_output.put_line('SUSPENSOS: ' || p_cont_sus || '. APROBADOS: ' || p_cont_ap || '. NOTABLES: ' || p_cont_not || '. SOBRESALIENTES: ' || p_cont_sob || '. ALUMNO CON LA MAYOR NOTA: ' || p_nom_max || ', NOTA: ' || p_nota_max || '. ALUMNO CON LA MENOR NOTA: ' || p_nom_min || ', NOTA: ' || p_nota_min);
 END MostrarResultados;
 /
